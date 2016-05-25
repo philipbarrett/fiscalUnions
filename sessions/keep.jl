d1763c91fbd255fcd3bd4277efb15094ff0c790b
Philip Barrett <pobarrett@gmail.com>
2016-05-25 09:32:20 -0500

    Updated keep (some stuff missing before)

--- sessions/keep.jl
+++ sessions/keep.jl
@@ -8,3 +8,10 @@ include("../julia/autarkySim.jl")
 am = AutarkyModel()
 as = solve_am(am)
 asim = sim_am(as)
+
+plot( asim[1:200,2] )
+plot( asim[1:200,4] )
+scatter( asim[1:200,2 ], asim[1:200,4 ] )
+
+cor( asim[1:(end-1),2], asim[2:(end),2])
+cor( asim[1:(end-1),4], asim[2:(end),4])
