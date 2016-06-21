f065dac548758d4aba542587d9f8f4eafdc19439
Philip Barrett <pobarrett@gmail.com>
2016-06-21 11:44:30 -0500

    Markov discretization complete

--- R/taxVAR.R
+++ R/taxVAR.R
@@ -2,19 +2,21 @@ library(readr)
 library(vars)
 library(mvtnorm)
 library(markovchain)
+library(expm)
 # library(tidyr)
 # library(dplyr)
 # library(lubridate)
 
-# Rcpp::sourceCpp('code/2016/fiscalUnions/R/rcpp/dists.cpp')
+Rcpp::sourceCpp('code/2016/fiscalUnions/R/rcpp/dists.cpp')
 
 ## 0. Script variables
 file <- '~/Dropbox/data/2016/fiscalUnions/taxRevenue.csv'
 cts <- c("Germany", "France")
-gov <- 'NES'  # Total tax revenue
-y.min <- 1980 # 1960
-nn <- 1000   # Simulation periods
-n.Z <- 10     # Number of discretized periods
+gov <- 'NES'      # Total tax revenue
+y.min <- 1980     # 1960
+nn <- 1000        # Simulation periods
+n.Z <- 20         # Number of discretized periods
+n.sim <- 1e8   # Simulation length for cross-check
 
 ## 1. Read the data
 df <- read_csv( file )
@@ -31,8 +33,10 @@ names(tax)[ ncol(tax)-0:1] <- sapply( names(tax)[ ncol(tax)-0:1], function(x) su
     # Adjust the names
 
 ## 2. A quick plot ##
-plot( tax$Year, tax[,cts[1]], type='l', ylim = c(30,45), col='blue', lwd=2 )
-lines( tax$Year, tax[,cts[2]], type='l', col='red', lwd=2 )
+plot( tax$Year[ tax$Year >= y.min ], tax[tax$Year >= y.min ,cts[1]], type='l', ylim = c(30,45), col='blue', lwd=2, 
+      xlim=c(y.min,max(tax$Year)), xlab='Year', ylab='Tax/GDP' )
+lines( tax$Year[tax$Year >= y.min ], tax[tax$Year >= y.min ,cts[2]], type='l', col='red', lwd=2 )
+legend( 'bottomright', cts, lwd=2, col=c('blue','red'), bty='n' )
 var.tax <- VAR( subset(tax, Year > y.min)[,cts] )
 
 ## 3. The demeaned version ##
@@ -57,8 +61,43 @@ Z <- kmeans( X, n.Z )$centers
     # The Z points
 T.p <- trans_prob( phi, X, eps, Z )
     # The transition probabilities
+
+## 4. Return to the version with means ##
 T.vals <- Z + rep( tax.means, each=n.Z )
     # Re-mean
+ergodic <- (T.p %^% 100)[1,]
+    # The ergodic distribution
+plot( T.vals, cex = ergodic * 10, main="Ergodic distribution" )
+    # Plot the ergodic distribution
+heatmap(T.p, Colv ="Rowv" )
+    # Heatmap of transition probabilities
 
-# X prime itself needs to be an array of depth number of countries
+## 5. Simulate the discretized process and compare to the VAR ##
+mc.tax <- new( "markovchain", byrow=TRUE, transitionMatrix=T.p, name="Tax" )
+    # The markov chain object
+sim.tax <- T.vals[as.numeric(rmarkovchain( n.sim, mc.tax ) ), ]
+rownames(sim.tax) <- NULL
+colnames(sim.tax) <- cts
+    # Simulated taxes
+plot( 1:40, sim.tax[ 1:40, 1 ], type='l', col='blue', ylim = c(30,45),
+      xlab='Sim pd', ylab='Tax/GDP', lwd=2 )
+lines( 1:40, sim.tax[ 1:40, 2 ], type='l', col='red', lwd=2 )
+legend( 'bottomright', cts, lwd=2, col=c('blue','red'), bty='n' )
+    # Quick plot
+var.mc <- VAR( sim.tax )
+    # Create the VAR on the simulated data
+par(mfrow = c(1, 2))
+for( cty in cts ){
+  barplot( rbind( var.mc$varresult[[cty]]$coefficients, 
+                var.tax$varresult[[cty]]$coefficients ), 
+         beside=TRUE, col=c('blue', 'red'), main=paste0(cty, ' VAR coefficients'),
+         legend.text=c('Markov Chain \nSimulation', 'Data'), 
+         args.legend =list(x='topleft',bty='n'), log='y' )
+}
+par(mfrow = c(1, 1))
+
+### Q: Can I use markovchainFit on the nearest-neighbour version of a simulation
+### from the VAR to generate an answer?
+
+
 
