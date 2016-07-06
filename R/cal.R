196ba13fc2d753e90af1dc2414df36c23d951e00
Philip Barrett <pobarrett@gmail.com>
2016-07-05 23:03:27 -0500

    Detrended real labo(u)r productivity

--- R/cal.R
+++ R/cal.R
@@ -12,6 +12,7 @@ library(mvtnorm)
 library(markovchain)
 library(expm)
 library(reshape)
+library(mFilter)
 
 Rcpp::sourceCpp('code/2016/fiscalUnions/R/rcpp/dists.cpp')
 source('code/2016/fiscalUnions/R/AR1disc.R')
@@ -19,11 +20,9 @@ source('code/2016/fiscalUnions/R/AR1disc.R')
 ## 0. Script variables
 gdp.file <- '~/Dropbox/data/2016/fiscalUnions/gdpDefANA.csv'
     # OECD annual GDP data
-hours.pw.file <- '~/Dropbox/data/2016/fiscalUnions/hrsPerWorker.csv'
-    # Annual hours per worker
-emp.file <- '~/Dropbox/data/2016/fiscalUnions/totalEmployment.csv'
-    # Total employment
-exp.file <- '~/Dropbox/data/2016/fiscalUnions/govtNomTaxANA.csv'
+alp.file <- '~/Dropbox/data/2016/fiscalUnions/labProd.csv'
+    # Labor productivity
+tax.file <- '~/Dropbox/data/2016/fiscalUnions/govtNomTaxANA.csv'
     # This data source (OECD ANAs) gives consistent measures of government 
     # revenue, expenditure, and deficits.  The only downside is that it has no 
     # German data before 1995.  For this, I need to stitch on the canned 
@@ -36,7 +35,8 @@ int.file <- '~/Dropbox/data/2016/fiscalUnions/interestRates.csv'
 debt.file <- '~/Dropbox/data/2016/fiscalUnions/govtDebt.csv'
     # Debt/GDP ratios
 cts <- c("Germany", "France")
-expd.code <- c( 'GTE', 'GS13')         # Total expenditure, general government
+alp.code <- c( 'T_GDPHRS', 'C')       # GDP per hour worked, current prices
+expd.code <- c( 'GTE', 'GS13')        # Total expenditure, general government
 nom.gdp.code <- c( 'B1_GA', 'C' )     # GDP, current prices
 gdp.def.code <- c( 'B1_GA', 'DOB' )   # GDP, deflator (could use expenditure deflator here)
 
@@ -60,34 +60,23 @@ exp <- TRUE
 ## 2. Read in the data
 names(cts) <- cts
 
-### 2.1 Hours per worker ###
-cts.hrs <- if( 'Germany' %in% cts ) c(cts, 'West Germany'='West Germany' ) else cts
-df.hrs <- read_csv( hours.pw.file )
-names( df.hrs )[8] <- 'Year'
-df.hrs$Year <- as.numeric(as.character(df.hrs$Year))
-df.hrs <- subset( df.hrs, Country %in% cts.hrs & Year >= y.min )
-df.hrs <- df.hrs[, c('Country', 'Employment status', 'Year', 'Unit', 'Value')]
-df.hrs$Value <- as.numeric(df.hrs$Value)
+### 2.1 GDP per hour worked ###
+df.nom.alp <- read_csv( alp.file )
+names( df.nom.alp )[8] <- 'Year'
+df.nom.alp <- subset( df.nom.alp, SUBJECT==alp.code[1] & MEASURE==alp.code[2] &
+                        Country %in% cts & Year >= y.min )
+df.nom.alp <- df.nom.alp[, c('Country', 'Subject', 'Measure', 'Year', 'Value')]
+df.nom.alp$Year <- as.numeric(df.nom.alp$Year)
+df.nom.alp$Value <- as.numeric(df.nom.alp$Value)
     # Drop needless columns
-hrs <- reshape( df.hrs, timevar='Country', 
-                    idvar=names(df.hrs)[2:(ncol(df.hrs)-1)], direction='wide' )
+nom.alp <- reshape( df.nom.alp, timevar='Country', 
+                    idvar=names(df.nom.alp)[2:(ncol(df.nom.alp)-1)], direction='wide' )
     # The nominal GDP dataframe
-rownames(hrs) <- NULL
-names(hrs)[ 4:ncol(hrs)] <- sapply( names(hrs)[ 4:ncol(hrs)], function(x) substr(x,7,nchar(x)) )
+rownames(nom.alp) <- NULL
+names(nom.alp)[ ncol(nom.alp)-0:1] <- sapply( names(nom.alp)[ ncol(nom.alp)-0:1], function(x) substr(x,7,nchar(x)) )
     # Adjust the names
-if( 'Germany' %in% cts ){
-  hrs.ratio <- mean( hrs$Germany / hrs$`West Germany`, na.rm=TRUE )
-  hrs$Germany[is.na(hrs$Germany)] <- hrs.ratio * hrs$`West Germany`[is.na(hrs$Germany)]
-      # Paste in the W German hours
-  with( hrs, plot( Year, France, type='l', lwd=2, col='red', ylab='Hours/worker',
-                   ylim=range( hrs[,4:6], na.rm = T ) ) )
-  with( hrs, lines( Year, Germany, type='l', lwd=2, col='blue' ) )
-  with( hrs, lines( Year, `West Germany`, type='l', lwd=2, col='blue', lty=2 ) )
-      # Quick plot
-}
 
-
-### 2.2 Nominal GDP ###
+### 2.3 Nominal GDP ###
 df.nom.gdp <- read_csv( gdp.file )
 df.nom.gdp <- subset( df.nom.gdp, TRANSACT==nom.gdp.code[1] & MEASURE==nom.gdp.code[2] &
                         Country %in% cts & Year >= y.min )
@@ -102,7 +91,7 @@ rownames(nom.gdp) <- NULL
 names(nom.gdp)[ ncol(nom.gdp)-0:1] <- sapply( names(nom.gdp)[ ncol(nom.gdp)-0:1], function(x) substr(x,7,nchar(x)) )
     # Adjust the names
 
-### 2.3 GDP deflator ###
+### 2.4 GDP deflator ###
 df.gdp.def <- read_csv( gdp.file )
 df.gdp.def <- subset( df.gdp.def, TRANSACT==gdp.def.code[1] & MEASURE==gdp.def.code[2] &
                         Country %in% cts & Year >= y.min )
@@ -116,3 +105,55 @@ gdp.def <- reshape( df.gdp.def, timevar='Country',
 rownames(gdp.def) <- NULL
 names(gdp.def)[ ncol(gdp.def)-0:1] <- sapply( names(gdp.def)[ ncol(gdp.def)-0:1], function(x) substr(x,7,nchar(x)) )
     # Adjust the names
+
+### 2.5 Nominal expenditures ###
+df.expd <- read_csv( tax.file )
+df.expd <- subset( df.expd, TRANSACT==expd.code[1] & SECTOR==expd.code[2] &
+                     Country %in% cts & Year >= y.min )
+df.expd <- df.expd[, c('Country', 'Transaction', 'Sector', 'Measure', 'Year', 'Unit', 'PowerCode', 'Value')]
+df.expd$Year <- as.numeric(df.expd$Year)
+df.expd$Value <- as.numeric(df.expd$Value)
+    # Drop needless columns
+expd <- reshape( df.expd, timevar='Country', idvar=names(df.expd)[2:(ncol(df.expd)-1)], direction='wide' )
+    # The nominal expenditure dataframe
+rownames(expd) <- NULL
+names(expd)[ ncol(expd)-0:1] <- sapply( names(expd)[ ncol(expd)-0:1], function(x) substr(x,7,nchar(x)) )
+    # Adjust the names
+
+### 2.6 Govt consumption expenditures ###
+df.gcons <- read.table( cons.file, sep="\t", header = TRUE )
+    # For some reason read_csv gives the wrong data back!!!
+df.gcons <- df.gcons[ -( nrow(df.gcons) - 0:4 ), ]
+df.gcons <- df.gcons[, -c(1,2,4) ]
+names( df.gcons )[1] <- 'Country'
+names( df.gcons )[-1] <- sapply( names( df.gcons )[-1], 
+                                 function(x) substr(x,2,5) )
+df.gcons <- subset( df.gcons, Country %in% cts )
+df.gcons[,-1] <- apply(df.gcons[,-1],2,as.numeric)
+    # Clean the data
+df.gcons.melt <- melt(df.gcons, id='Country' ) 
+names( df.gcons.melt )[2:3] <- c('Year', 'GovtCons')
+for( i in 2:3 ) df.gcons.melt[,i] <- as.numeric( as.character( df.gcons.melt[,i ] ) )
+    # Melt down the data
+gcons <- reshape( subset( df.gcons.melt, Year >= y.min & Country %in% cts ), timevar='Country', 
+                  idvar='Year', direction='wide' )
+    # Reshape as required
+rownames(gcons) <- NULL
+names(gcons)[ ncol(gcons)-0:1] <- sapply( names(gcons)[ ncol(gcons)-0:1], function(x) substr(x,10,nchar(x)) )
+gcons[,cts] <- gcons[,cts] / 1e6
+    # Adjust the names
+
+## 3. Create single-country VARs
+
+## 3.1 Create and filter real ave Labor Prod'y
+log.real.alp <- data.frame( Year=nom.alp$Year, log( nom.alp[,cts]/ gdp.def[,cts] * 100 ) )
+    # Log real ave lab prod
+real.alp.hp <- apply( log.real.alp[,cts], 2, hpfilter,
+                      freq=6.25, type="lambda", drift=FALSE )
+real.alp.trend <- data.frame( Year=nom.alp$Year, sapply( real.alp.hp, function(x) x$trend ) )
+real.alp.cycle <- data.frame( Year=nom.alp$Year, sapply( real.alp.hp, function(x) x$cycle ) )
+    # Trend and cycle ALP
+
+
+
+
