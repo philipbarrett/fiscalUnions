150a9c95c1e5cc4a363572a000196d880aa416e4
Philip Barrett <pobarrett@gmail.com>
2016-07-13 22:36:51 -0500

    More iterations
    Also did some profiling.  Didn't help :(

--- sessions/example.jl
+++ sessions/example.jl
@@ -20,13 +20,13 @@ P = [ .4 .2 .2 .2
       .2 .4 .2 .2
       .2 .2 .4 .2
       .2 .2 .2 .4 ]
-nR = 10
+nR = 20
 psi = .75
 chi = 7.0
-nb=10
+nb = 20
 r = .03
-delta= 1.0
-
+delta = 1.0
+ndirsl = 8
 
 # dw = DWL( A=A, g=g, nR=nR, psi=psi, chi=chi, r=r )
 # am = AutarkyModel( r=r, delta=delta, psi=psi, chi=chi, nR=nR )
@@ -49,7 +49,7 @@ delta= 1.0
 # bellman_operator!(pm, V,vOut, bprime, R1, R2, x1, x2 )
 
 fg = FiscalGame( r=r, delta=[delta, delta], psi=[ psi, psi ], chi=[ chi, chi ],
-                A=A_jt, g=g_jt, P=P, nR=nR, rho=.5, nb=nb )
+              A=A_jt, g=g_jt, P=P, nR=nR, rho=.5, nb=nb, ndirsl=ndirsl )
 init, ndirs = initGame(fg)
 polyPlot(init[:,1])
 
@@ -58,59 +58,56 @@ thisndirs = 20
 dirs = hcat( [ cos(2*pi*(i-1)/thisndirs)::Float64 for i in 1:thisndirs ],
              [ sin(2*pi*(i-1)/thisndirs)::Float64 for i in 1:thisndirs ] )
 
-ff = uncSetUpdate( fg.surp[3,2], vec(fg.pdLoss[3,:,2]), fg.bgrid[4],
-                         r, fg.bgrid, init, vec(P[2,:]), betta_hat,
-                         dirs )
-    # Compilation stage
-Profile.clear()
-@profile ff = uncSetUpdate( fg.surp[3,2], vec(fg.pdLoss[3,:,2]), fg.bgrid[4],
-                        r, fg.bgrid, init, vec(P[2,:]), betta_hat,
-                        dirs )
-Profile.print()
-
-gg = uncSetUpdate( fg.surp[3,2], vec(fg.pdLoss[3,:,2]), fg.bgrid[4],
-                        r, fg.bgrid, init, vec(P[2,:]), betta_hat,
-                        dirs, false )
-polyPlot([ff, gg])
-
-hh = uncSetUpdate( fg.surp[1,1], vec(fg.pdLoss[1,:,1]), fg.bgrid[1],
-                  r, fg.bgrid, init, vec(P[1,:]), betta_hat, dirs )
-iS = 1
-ib = 10
+# ff = uncSetUpdate( fg.surp[3,2], vec(fg.pdLoss[3,:,2]), fg.bgrid[4],
+#                          r, fg.bgrid, init, vec(P[2,:]), betta_hat,
+#                          dirs )
+# gg = uncSetUpdate( fg.surp[3,2], vec(fg.pdLoss[3,:,2]), fg.bgrid[4],
+#                         r, fg.bgrid, init, vec(P[2,:]), betta_hat,
+#                         dirs, false )
+# polyPlot([ff, gg])
+#
+# hh = uncSetUpdate( fg.surp[1,1], vec(fg.pdLoss[1,:,1]), fg.bgrid[1],
+#                   r, fg.bgrid, init, vec(P[1,:]), betta_hat, dirs )
+# iS = 1
+# ib = 10
 # for k in find(fg.potFeas[is,ib])
 #   println( "k=", k )
 #   jj = uncSetUpdate( fg.surp[k,is], vec(fg.pdLoss[k,:,is]), fg.bgrid[ib],
 #                                     fg.r, fg.bgrid, init, vec(fg.P[is,:]),
 #                                     betta_hat, fg.dirs )
 # end
-
-Profile.clear()
-@profile kk = [ uncSetUpdate( fg.surp[k,is], vec(fg.pdLoss[k,:,is]), fg.bgrid[ib],
-                                  fg.r, fg.bgrid, init, vec(fg.P[is,:]), betta_hat,
-                                  dirs )::Polygon
-                      for k in find(fg.potFeas[iS,ib]) ]
-Profile.print()
+#
+# kk = [ uncSetUpdate( fg.surp[k,iS], vec(fg.pdLoss[k,:,iS]), fg.bgrid[ib],
+#                                   fg.r, fg.bgrid, init, vec(fg.P[iS,:]), betta_hat,
+#                                   dirs )::Polygon
+#                       for k in find(fg.potFeas[iS,ib]) ]
+#     # Compilation
+# using BenchmarkTools
+# bmk = @benchmark kk = [ uncSetUpdate( fg.surp[k,iS], vec(fg.pdLoss[k,:,iS]), fg.bgrid[ib],
+#                                   fg.r, fg.bgrid, init, vec(fg.P[iS,:]), betta_hat,
+#                                   dirs )::Polygon
+#                                   for k in find(fg.potFeas[iS,ib]) ]
+#     # Averages about 3.5ms (0.0035s)
+#     # Following aggresive typing in Polygons, this went *up*
+# println(bmk)
 
 # W = uncSetUpdate( fg, init, ndirs )
 W = init
-# W_new = uncSetUpdate( fg, W, ndirs )
-#     # Triggers compilation
-#   # in case we have any previous profiling data
-# @profile W_new = uncSetUpdate( fg, W, ndirs )
-# using ProfileView
-# ProfileView.view()
 
+it = 0
+hd = 10
 
-# for it in 1:5
-#   println("*** it = ", it , " ***")
-#   W_new = uncSetUpdate( fg, W, ndirs )
-#   hd = hausdorff( W, W_new )
-#   println("   ** mean & max hausdorff dist = ", ( mean(hd), maximum(hd) ), "**" )
-#   W = copy(W_new)
-#   ndirs = ndirsUpdate( ndirs, hd, fg.hddirs, fg.ndirsu )
-#   println("   ** mean # search dirs = ", mean(ndirs), "**" )
-#   println("   ** extremal # search dirs = ", extrema(ndirs), "**\n" )
-# end
+while it < 80 && hd > 1e-05
+  it += 1
+  println("*** it = ", it , " ***")
+  W_new = uncSetUpdate( fg, W, ndirs )
+  hd = hausdorff( W, W_new )
+  println("   ** mean & max hausdorff dist = ", ( mean(hd), maximum(hd) ), "**" )
+  W = copy(W_new)
+  ndirs = ndirsUpdate( ndirs, hd, fg.hddirs, fg.ndirsu )
+  println("   ** mean # search dirs = ", mean(ndirs), "**" )
+  println("   ** extremal # search dirs = ", extrema(ndirs), "**\n" )
+end
 
 
 
